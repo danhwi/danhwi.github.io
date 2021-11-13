23b87555d963b17efa549b206af7a8fae6e715ed
danhwi <danhwi.jung@gmail.com>
2021-11-14 03:44:13 +0900

    Trace of facebook

---
+++ src/routes/trace/act-agile.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Post from '../../components/Post.svelte';
+
+    let category = 'trace';
+    let title = '연기는 Agile이다?';
+    let create = '2020-08-02';
+</script>
+
+<style>
+    em {
+        color: gray;
+    }
+</style>
+
+<Post {category} {title} {create}>
+    <p>
+        요구사항―대본을 바탕으로 분석하고 설계한다.
+        그것을 바탕으로 실행 가능한 형태의 코드―연기를 구현하고 실행한다.
+        실행 결과는 분석·설계한 것을 바탕으로 확인하고 검증한다.
+    </p>
+    <p>
+        결국 그런 이야기.
+        <em>분석 및 설계 → 구현 및 실행 → 확인 및 검증, 그런 프로세스.</em>
+        그리고 이것은 단 하나의 waterfall이 아니라
+        여러 번의 iteration으로 반복 수행된다.
+        처음부터 모든 것을 해내려고 하기 보다는 조금씩 적용해가며
+        반복할 때마다 확장적으로 개선되어 간다.
+    </p>
+    <p>
+        그건 마치 Iterative Spiral Model... 결국 연기는 Agile이다?&#x1F3AD;
+    </p>
+    <p class="quote">
+        &#x1F517; <a href="https://www.facebook.com/danhwi.jung/posts/1577522255759587">페이스북 게시물</a>에서 발췌
+    </p>
+</Post>
---
+++ src/routes/trace/software-engineering.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Post from '../../components/Post.svelte';
+
+    let category = 'trace';
+    let title = '소프트웨어 공학적 관점';
+    let create = '2020-08-28';
+</script>
+
+<Post {category} {title} {create}>
+    <p>
+        정말 사소한 것 하나를 작동시킨다 하더라도
+    </p>
+    <p>
+        구현하기 전엔 실행하지 못할 것이요,<br />
+        설계하기 전엔 구현하지 못할 것이요,<br />
+        분석하기 전엔 설계하지 못할 것이요,<br />
+        이해하기 전엔 분석하지 못할 것이다.
+    </p>
+    <p>
+        인간이란 결국
+        임베디드 하드웨어와 임베디드 소프트웨어로 구성된
+        임베디드 시스템.
+    </p>
+    <p>
+        ... 시스템 소프트웨어랑 소프트웨어 공학
+        너무 좋아하는 것 같다.
+    </p>
+    <p>
+        하지만 정말 호흡이라는 Tick과 충동이라는 Trigger로,
+        사실 그렇잖아.
+        고대 그리스에선 공학과 예술은 본디 하나였는걸?&#x1F3AD;
+    </p>
+    <p class="quote">
+        &#x1F517; <a href="https://www.facebook.com/danhwi.jung/posts/1600992953412517">페이스북 게시물</a>에서 발췌
+    </p>
+</Post>
